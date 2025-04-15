b025dd723684914749571b638855e09c45ad81a4
AguLeon <leon.agustin@gmail.com>
2025-04-14 22:37:19 -0400

    Pushing fresh content.

--- src/components/Right/About/About.jsx
+++ src/components/Right/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       aria-label='About me'
     >
       {/* color bank: light green (safe option): 3e5044; darker green: 0f2418; olive: 2e3d2f; charcoal: 1a1a1a; slate deep blue: 1f2f2d   */}
-      <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-[#0f2418]/50 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+      <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-[#262e29]/50 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
         <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>
           About
         </h2>
